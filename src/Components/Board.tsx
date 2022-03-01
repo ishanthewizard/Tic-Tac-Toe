94ac9734f35ffac8193fae86080f07d82bb7323f
ishanthewizard <77704916+ishanthewizard@users.noreply.github.com>
2022-02-28 18:44:17 -0800

    commit during work session

--- src/Components/Board.tsx
+++ src/Components/Board.tsx
@@ -2,18 +2,19 @@ import * as React from 'react';
 import {Box, Flex} from '@chakra-ui/react';
 import Tile from './Tile'
 function Board() {
+    
+    function genTiles(n : number) {
+        const result : (JSX.Element)[] = [];
+        for(let i = 0; i < n; i++) {
+            result.push(<Tile num = {i}/>);
+        }
+        return result;
+    }
+    const tileArray: (JSX.Element)[] = genTiles(9);
     return (
         <Box className = "board">
             <Flex justify= "center" wrap= "wrap">
-                <Tile/>
-                <Tile/>
-                <Tile/>
-                <Tile/>
-                <Tile/>
-                <Tile/>
-                <Tile/>
-                <Tile/>
-                <Tile/>
+                {tileArray}
             </Flex>
 
 
